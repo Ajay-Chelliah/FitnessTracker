86218a48bc31515f166b62f3bc6f60be152cfd4a
Ajay-Chelliah <ajay8425p@gmail.com>
2024-10-13 16:31:37 +0530

    Created the data_resample.pkl file

--- src/components/Data_Ingestion.py
+++ src/components/Data_Ingestion.py
@@ -6,23 +6,23 @@ from glob import glob
 # --------------------------------------------------------------
 
 single_file_acc = pd.read_csv(
-    "../notebook/Data/Raw/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+    "../../notebook/Data/Raw/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
 )
 single_file_gyro = pd.read_csv(
-    "../notebook/Data/Raw/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+    "../../notebook/Data/Raw/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
 )
 # --------------------------------------------------------------
 # List all data in data\raw
 # --------------------------------------------------------------
 
-files = glob("../notebook/Data/Raw/*.csv")
+files = glob("../../notebook/Data/Raw/*.csv")
 file_len = len(files)
 
 # --------------------------------------------------------------
 # Extract features from filename
 # --------------------------------------------------------------
 
-data_path = "../notebook/Data/Raw\\"
+data_path = "../../notebook/Data/Raw\\"
 f = files[0]
 participant = f.split("-")[0].replace(data_path, "")
 label = f.split("-")[1]
@@ -67,7 +67,20 @@ for f in files:
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+acc_df.info()
+pd.to_datetime(df["epoch (ms)"], unit="ms")
+pd.to_datetime(df["time (01:00)"]).dt.day_of_week
 
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyro_df.index = pd.to_datetime(gyro_df["epoch (ms)"], unit="ms")
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyro_df["epoch (ms)"]
+del gyro_df["time (01:00)"]
+del gyro_df["elapsed (s)"]
 
 # --------------------------------------------------------------
 # Turn into function
@@ -77,7 +90,19 @@ for f in files:
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
-
+data_merged = pd.concat([acc_df.iloc[:, :3], gyro_df], axis=1)
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyro_x",
+    "gyro_y",
+    "gyro_z",
+    "label",
+    "category",
+    "participant",
+    "set",
+]
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
@@ -86,7 +111,34 @@ for f in files:
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
 
+sampling = {
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyro_x": "mean",
+    "gyro_y": "mean",
+    "gyro_z": "mean",
+    "label": "last",
+    "category": "last",
+    "participant": "last",
+    "set": "last",
+}
 
+data_merged[:1000].resample(rule="200ms").apply(sampling)
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
+
+data_resample = pd.concat(
+    [df.resample(rule="200ms").apply(sampling).dropna() for df in days]
+)
+data_resample.info()
+data_resample["set"] = data_resample["set"].astype("int")
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+data_resample.to_pickle("../../artifacts/data_resample.pkl")
+
+# save_object(
+#     file_path='../../artifacts/Data_resample.pkl',
+#     obj=data_resample
+# )
--- src/utils.py
+++ src/utils.py
@@ -1,47 +1,48 @@
-import os
-import sys
+# import os
+# import sys
 
-import numpy as np
-import pandas as pd
-import dill
-from sklearn.metrics import r2_score
+# import numpy as np
+# import pandas as pd
+# import dill
+# # from sklearn.metrics import r2_score
 
-from src.exception import CustomException
+# from src.exception import CustomException
 
-def save_object(file_path,obj):
-    try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
-    
-    except Exception as e:
-        raise CustomException(e,sys)
-    
-def load_object(file_path):
-    try:
-        with open(file_path,'rb') as file_obj:
-            return dill.load(file_obj)
-    except Exception as e:
-        raise CustomException(e, sys)
+# def save_object(file_path, obj):
+#     try:
+#         dir_path = os.path.dirname(file_path)
+#         os.makedirs(dir_path, exist_ok=True)
 
-def evaluate_model(X_train, Y_train , X_test , Y_test ,models):
-    try:
-        report = {}
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            model.fit(X_train,Y_train)
+#         with open(file_path, "wb") as file_obj:
+#             dill.dump(obj, file_obj)
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
-            
-            train_model_score = r2_score(Y_train,y_train_pred)
-            test_model_score = r2_score(Y_test,y_test_pred)
+#     except Exception as e:
+#         raise CustomException(e, sys)
 
-            report[list(models.keys())[i]] = test_model_score
-        
-        return report
+# def load_object(file_path):
+#     try:
+#         with open(file_path,'rb') as file_obj:
+#             return dill.load(file_obj)
+#     except Exception as e:
+#         raise CustomException(e, sys)
 
-    except Exception as e:
-        raise CustomException(e,sys)
+# def evaluate_model(X_train, Y_train , X_test , Y_test ,models):
+#     try:
+#         report = {}
+#         for i in range(len(list(models))):
+#             model = list(models.values())[i]
+#             model.fit(X_train,Y_train)
+
+#             y_train_pred = model.predict(X_train)
+#             y_test_pred = model.predict(X_test)
+
+#             train_model_score = r2_score(Y_train,y_train_pred)
+#             test_model_score = r2_score(Y_test,y_test_pred)
+
+#             report[list(models.keys())[i]] = test_model_score
+
+#         return report
+
+# except Exception as e:
+#     raise CustomException(e, sys)
